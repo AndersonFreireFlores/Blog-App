3224ef396349742c7a49f7dcefd7f918883400c5
Anderson Freire Flores <andersonfflores75@gmail.com>
2024-07-18 21:39:39 -0300

    User repository

---
+++ src/main/java/andersonfflores/blogapp/Repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package andersonfflores.blogapp.Repositories;
+
+import andersonfflores.blogapp.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
