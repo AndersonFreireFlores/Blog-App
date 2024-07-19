bb5d0d5a7b56c7ea79d4b453bb8727b020284ccf
Anderson Freire Flores <andersonfflores75@gmail.com>
2024-07-19 19:57:04 -0300

    Post Controller

---
+++ src/main/java/andersonfflores/blogapp/PostController.java
@@ -0,0 +1,44 @@
+package andersonfflores.blogapp;
+
+import andersonfflores.blogapp.Models.DTOs.PostDTO;
+import andersonfflores.blogapp.Repositories.PostRepository;
+import andersonfflores.blogapp.Services.PostService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController("/post")
+public class PostController {
+
+   private final PostService postService;
+
+    public PostController(PostService service) {
+        this.postService = service;
+    }
+
+    @GetMapping
+    public List<PostDTO> getAllPosts(){
+        return postService.getAllPosts();
+    }
+
+    @GetMapping("/{id}")
+    public PostDTO getPostById(UUID id){
+        return postService.getPostById(id);
+    }
+
+    @PostMapping
+    public PostDTO createPost(@RequestBody PostDTO post){
+        return postService.savePost(post);
+    }
+
+    @PutMapping("/{id}")
+    public PostDTO updatePost(@PathVariable UUID id, @RequestBody PostDTO post){
+        return postService.updatePost(id, post);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePost(@PathVariable UUID id){
+        postService.deletePost(id);
+    }
+}
--- src/main/java/andersonfflores/blogapp/Repositories/CommentRepository.java
+++ src/main/java/andersonfflores/blogapp/Repositories/CommentRepository.java
@@ -2,7 +2,6 @@ package andersonfflores.blogapp.Repositories;
 
 import andersonfflores.blogapp.Models.Comment;
 import andersonfflores.blogapp.Models.DTOs.CommentDTO;
-import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
